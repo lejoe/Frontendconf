f6773955237a7f89b6828ad7399fcd4b0b847048
Memi Beltrame <memi.beltrame@liip.ch>
2011-06-23 07:51:49 +0200

    fix typo

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -10,6 +10,6 @@
                 <li><span><a class="naviItem">Speakers & Sessions  </a></span><span class="next">></span></li>
                 <li><span>Location </span><span class="next">></span></li>
                 <li><span>Tickets </span><span class="next">></span></li>
-                <li><span>Accomodation </span><span class="next">></span></li>
+                <li><span>Acomodation </span><span class="next">></span></li>
                 <li><span>Sponsors </span><span class="next">></span></li>
             </ul>
