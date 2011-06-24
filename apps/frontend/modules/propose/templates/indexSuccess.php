195891b9fe74285d0491aa67b3861c3feb9e5947
Memi Beltrame <memi.beltrame@liip.ch>
2011-06-24 19:22:36 +0200

    style form

--- apps/frontend/modules/propose/templates/indexSuccess.php
+++ apps/frontend/modules/propose/templates/indexSuccess.php
@@ -1,43 +1,52 @@
 <div class="row lastBeforeFooter">
     <div class="column grid_6">
         <h2>Propose a talk</h2>
-
+        <form action="/propose/add/"  method="post">
         <fieldset>
-            <legend>Your Talk:</legend>
-            <label for="name">Talk title</label><br>
-            <input type="text" name="name" class="maxWidth"><br>
-            <label for="profession">Abstract</label><br>
-            <textarea></textarea>
+            <legend>Tell us about Your Talk</legend>
+            <p class="fsContent">
+
+                <label for="title">Talk title</label>
+                <input type="text" name="title" class="maxWidth"><br>
+                <label for="abstract">Abstract</label>
+                <textarea name="abstract" class="maxWidth" rows="15"></textarea>
+            </p>
         </fieldset>
 
         <fieldset>
-            <legend>Tell us about you:</legend>
-            <label for="name">Name</label><br>
-            <input type="text" name="name" class="maxWidth"><br>
-            <label for="profession">Profession</label><br>
-            <input type="text" name="profession" class="maxWidth"><br>
-            <label for="name">Company</label><br>
-            <input type="text" name="name" class="maxWidth"><br>
-            <label for="name">Email</label><br>
-            <input type="email" name="email" class="maxWidth"><br>
-            <label for="name">Twitter-Name</label><br>
-            <input type="text" name="name" class="maxWidth"><br>
-
-            <label for="name">Your Website</label><br>
-            <input type="text" name="name" class="maxWidth"><br>
+            <legend>Tell us about you</legend>
+            <p class="fsContent">
+                <label for="name">Name</label>
+                <input type="text" name="name" class="maxWidth"><br>
+                <label for="profession">Profession</label>
+                <input type="text" name="profession" class="maxWidth"><br>
+                <label for="name">Company</label>
+                <input type="text" name="name" class="maxWidth"><br>
+                <label for="name">Email</label>
+                <input type="email" name="email" class="maxWidth"><br>
+                <label for="name">Twitter-Name</label>
+                <input type="text" name="name" class="maxWidth"><br>
+                <label for="name">Your Website</label>
+                <input type="text" name="name" class="maxWidth"><br>
+            </p>
         </fieldset>
 
         <fieldset>
             <legend>Orgnisational Stuff</legend>
-            <label for="name">Do you have a preference for the Date of your talk?</label><br>
-            <input type="radio" name="preference" value="none"> None
-            <input type="radio" name="preference" value="none"> Friday
-            <input type="radio" name="preference" value="none"> Saturday <br>
-            <label for="name">Does your company pay for your expenses?</label><br>
-            <input type="radio" name="preference" value="none"> Yes
-            <input type="radio" name="preference" value="none"> No
-            <input type="radio" name="preference" value="none"> I don't know<br>
+            <p class="fsContent">
+                <label for="name">Do you have a preference for the Date of your talk?</label><br>
+                <input type="radio" name="preference" value="none"> None
+                <input type="radio" name="preference" value="none"> Friday
+                <input type="radio" name="preference" value="none"> Saturday <br><br>
+                <label for="name">Does your company pay for your expenses?</label><br>
+                <input type="radio" name="preference" value="none"> Yes
+                <input type="radio" name="preference" value="none"> No
+                <input type="radio" name="preference" value="none"> I don't know<br>
+            </p>
         </fieldset>
+        <br>
+        <input class="button" type="submit" value="Submit Proposal">
+        </form>
     </div>
     <div class="column grid_6">
 
