0369fff6a9d4d92a891f928660d169ceaa83b73e
Memi Beltrame <memi.beltrame@liip.ch>
2011-06-24 19:22:27 +0200

    change button label

--- apps/frontend/modules/tickets/templates/indexSuccess.php
+++ apps/frontend/modules/tickets/templates/indexSuccess.php
@@ -9,7 +9,7 @@
             Leave us your email-address and we will inform you once we're ready.<br>
             <form action ="/tickets/rememberMe/" method="post">
                 <input type="email" name="email">
-                <input type="submit" value="Go">
+                <input class="button" type="submit" value="Send">
             </form>
         </div>
 
