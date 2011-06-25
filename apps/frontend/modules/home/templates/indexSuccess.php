707f3d125c83115097f2ac0ad0bdf5a432b0761a
Memi Beltrame <memi.beltrame@liip.ch>
2011-06-26 01:18:40 +0200

    add speakers and news to front

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -8,9 +8,79 @@
         </div>
         <div class="row lastBeforeFooter">
             <div class="column grid_6">
-                &nbsp;
+
+                <!-- Speakers -->
+
+                <div class="row">
+                    <div class="column grid_6">
+                        <h3 class="front">&lt;speakers></h3>
+                    </div>
+                </div>
+                <div class="row speakerEntry">
+                    <div class="column innergrid_1">
+                        <img class="person" src="/images/lea.png">
+                    </div>
+                    <div class="column grid_5">
+                        <h4>Lea Verou</h4>
+                        Web developer. Passionate about open web standards, especially CSS3, HTML5 or ECMAScript. Co-founder of Fresset Ltd. Loves good design. Proficient in nerding.
+                    </div>
+                </div>
+                <div class="row speakerEntry">
+                    <div class="column innergrid_1">
+                        <img class="person" src="/images/jcf.png">
+                    </div>
+                    <div class="column grid_5">
+                        <h4>Jens Christian Fischer</h4>
+                        tinkerer, practician, putting things together and make them work.
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="column grid_6">
+                        <h3 class="close">&lt;speakers/></h3>
+                    </div>
+                </div>
             </div>
             <div class="column grid_6">
                 <a class="button" href="/propose">Propose a Talk</a>
+                <br><br>
+
+                <!-- news -->
+
+                <div class="row">
+                    <div class="column grid_6">
+                        <h3 class="news">&lt;news></h3>
+                    </div>
+                </div>
+                <div class="row newsBG news">
+                    <div class="column innergrid_1 newsdate">
+                        <br>
+                        <span class="day">25</span><br>
+                        <span class="month">Jun</span>
+
+                    </div>
+                    <div class="column grid_5">
+                        <h4>Site online</h4>
+                        Finally our site went online - you can now sumbit proposals and have a peek at the speakers already confirmed.
+                    </div>
+                </div>
+                <div class="row newsBG news">
+                    <div class="column innergrid_1 newsdate">
+                        <br>
+                        <span class="day">22</span><br>
+                        <span class="month">Jun</span>
+
+                    </div>
+                    <div class="column grid_5">
+                        <h4>Frontend Conference Zurich </h4>
+                        The first Frontend Conference Zürich has been announced. Yay!
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="column grid_6">
+                        <h3 class="news newsClose">&lt;news/></h3>
+                    </div>
+                </div>
+
             </div>
         </div>
