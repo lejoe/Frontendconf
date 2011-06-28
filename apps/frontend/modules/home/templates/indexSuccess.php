ee9cf61c7cf4e44e6d298f94c05cf51ec1432607
Memi Beltrame <memi.beltrame@liip.ch>
2011-06-28 21:03:42 +0200

    add twitter widget, do some styling

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -73,6 +73,50 @@
                         <h3 class="news newsClose">&lt;news/></h3>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="column grid_6">
+                        <br><br>
+                        <h3 class="front">Twitter</h3>
+
+
+                        <script src="http://widgets.twimg.com/j/2/widget.js"></script>
+                        <script>
+                            new TWTR.Widget({
+                            version: 2,
+                            type: 'profile',
+                            rpp: 4,
+                            interval: 6000,
+                            width: 'auto',
+                            height: 200,
+                            theme: {
+                            shell: {
+                              background: 'none',
+                              color: '#ffffff'
+                            },
+                            tweets: {
+                              background: 'none',
+                              color: '#49422f',
+                              links: '#0e3861'
+                            }
+                            },
+                            features: {
+                            scrollbar: false,
+                            loop: false,
+                            live: true,
+                            hashtags: true,
+                            timestamp: true,
+                            avatars: false,
+                            behavior: 'all'
+                            }
+                            }).render().setUser('frontendconfch').start();
+                        </script>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="column grid_6">
+                        <h3 class="close">&lt;twitter/></h3>
+                    </div>
+                </div>
 
             </div>
         </div>
