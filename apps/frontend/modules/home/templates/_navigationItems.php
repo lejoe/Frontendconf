54b6b9b4e770207a738dc92cf1b399483671bd62
lejoe <copernic67@lejoe.com>
2011-07-01 23:39:14 +0200

    Made the navigation validate

--- apps/frontend/modules/home/templates/_navigationItems.php
+++ apps/frontend/modules/home/templates/_navigationItems.php
@@ -1,5 +1,5 @@
-<a class="naviItem" href="/speakers"><li><span>Speakers </span><span class="next">></span></li></a>
-<a class="naviItem" href="/propose"><li><span>Propose a talk </span><span class="next">></span></li></a>
-<a class="naviItem" href="/tickets"><li><span>Tickets</span><span class="next">></span></li></a>
-<a class="naviItem" href="/venue"><li><span>Venue </span><span class="next">></span></li></a>
-<a class="naviItem" href="/sponsoring"><li class="last"><span>Sponsoring</span><span class="next">></span></li></a>
+<li><a class="naviItem" href="/speakers"><span>Speakers </span><span class="next">></span></a></li>
+<li><a class="naviItem" href="/propose"><span>Propose a talk </span><span class="next">></span></a></li>
+<li><a class="naviItem" href="/tickets"><span>Tickets</span><span class="next">></span></a></li>
+<li><a class="naviItem" href="/venue"><span>Venue </span><span class="next">></span></a></li>
+<li class="last"><a class="naviItem" href="/sponsoring"><span>Sponsoring</span><span class="next">></span></a></li>
