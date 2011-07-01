a2c3b0d07006c1b8e575965d06e0bb4383b0f1e7
Memi Beltrame <memi.beltrame@liip.ch>
2011-07-01 17:33:31 +0200

    swapping sponors and sponsoring to avoid confusion

--- apps/frontend/modules/home/templates/_navigationItems.php
+++ apps/frontend/modules/home/templates/_navigationItems.php
@@ -2,4 +2,4 @@
 <a class="naviItem" href="/propose"><li><span>Propose a talk </span><span class="next">></span></li></a>
 <a class="naviItem" href="/tickets"><li><span>Tickets</span><span class="next">></span></li></a>
 <a class="naviItem" href="/venue"><li><span>Venue </span><span class="next">></span></li></a>
-<a class="naviItem" href="/sponsors"><li class="last"><span>Sponsors </span><span class="next">></span></li></a>
+<a class="naviItem" href="/sponsoring"><li class="last"><span>Sponsoring</span><span class="next">></span></li></a>
--- apps/frontend/modules/sponsors/templates/indexSuccess.php
+++ apps/frontend/modules/sponsors/templates/indexSuccess.php
@@ -1,6 +1,6 @@
 <div class="row ">
     <div class="column grid_12">
-        <h2>Sponsors Information</h2>
+        <h2>Sponsors</h2>
     </div>
 </div>
 <div class="row lastBeforeFooter">
@@ -8,8 +8,7 @@
         <div class="lead">
             Our goal is to initiate and establish the Frontend Conference Zurich  as a leading conference about state-of-the-art frontend and UX technologies in Europe and as the leading frontend/UX conference in Switzerland.<br><br>
         </div>
-        The conference is an excellent platform for Swiss and European scientists, developers and designers to present their projects, latest research and technologies in the fields of Web technologies, Web design and UX/HCI (Human Computer Interaction). We'll offer an opportunity to network with and learn from peers and leading experts in their fields, appreciating contributions both from practitioners and academics likewise. Zurich, conveniently located in the heart of Europe, with the University of Zurich (UZH), Federal Institute of Technology (ETHZ) and other institutions of higher education and a strong and flourishing IT sector is an ideal place to establish this conference.
-    </div>
+     </div>
     <div class="column grid_4">
         <div class="infobox">
             <h3>Get all the Sponsor Info</h3>
@@ -22,49 +21,27 @@
             Markus Leutwyler<br>
             markus < at > frontendconf.ch<br>
             twitter.com/twtomcat<br>
-            Phone +41 78 697 79 89
         </div>
     </div>
 </div>
+<!-- Platinum -->
 <div class="row lastBeforeFooter">
-    <div class="column grid_4">
+    <div class="column grid_8">
         <h3>Platinum</h3>
-        <ul class="point">
-            <li>There will only be two Platinum sponsors in total.</li>
-            <li>1 Room named like the sponsor (with banner)</li>
-            <li>Logo on web site, web site footer, e-mail footer and program guide</li>
-            <li>Logo, link & description on conference's sponsoring web page</li>
-            <li>Dedicated slide for the conference's sponsoring presentation slides</li>
-            <li>6 free tickets</li>
-            <li>> Hiring corner: A3 paper</li>
-            <li>Program guide ad: Half page</li>
-            <li>Contribution to the goodie bag</li>
-        </ul>
-        CHF 10'000.-
     </div>
     <div class="column grid_4">
-        <h3>Gold</h3>
-
-        <ul class="point">
-            <li>Logo on web site and program guide</li>
-            <li>Logo, link & description on conference's sponsoring web page</li>
-            <li>Shared (2x) slide for the conference's sponsoring presentation slides</li>
-            <li>3 free tickets</li>
-            <li>Hiring corner: A4 paper</li>
-            <li>Program guide ad: Quarter page</li>
-            <li>Contribution to the goodie bag</li>
-        </ul>
-        CHF 5'000.-
+        &nbsp;
     </div>
-    <div class="column grid_4">
-        <h3>Silver</h3>
-        <ul class="point">
-            <li>Logo on web site and program guide</li>
-            <li>Logo, link & description on conference's sponsoring web page</li>
-            <li>Shared (3x) slide for the conference's sponsoring presentation slides</li>
-            <li>Contribution to the goodie bag</li>
-        </ul>
-            CHF 2’500.-
+</div>
+<div class="row lastBeforeFooter">
+    <div class="column grid_3">
+        <img src="/images/logo_acme.png">
     </div>
-
+    <div class="column grid_7">
+        <h3>Acme</h3>
+        Lorem Ipsum
+    </div>
+    <div class="column grid_2">
+        &nbsp;
+    </div>
 </div>
