89f6a20c88299dffb7a9a7a1f826cb943cc640d5
Memi Beltrame <memi.beltrame@liip.ch>
2011-07-04 18:37:07 +0200

    remove metatag

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -5,12 +5,12 @@
         <link href="http://fonts.googleapis.com/css?family=Copse&v1" rel="stylesheet" type="text/css">
         <link href="http://fonts.googleapis.com/css?family=Droid+Sans&v1" rel="stylesheet" type="text/css">
         <link href="http://fonts.googleapis.com/css?family=Anton&v1" rel="stylesheet" type="text/css"><meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-        <meta name="viewport" content=" minimum-scale=1.0, maximum-scale=1.0, width=device-width; user-scalable=no">
+
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
     <link rel="shortcut icon" href="/favicon.png" />
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" >
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js" ></script>
 
     <?php include_javascripts() ?>
     <script type="text/javascript">
