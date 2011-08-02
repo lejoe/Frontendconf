9df6e55ccfeb50d938382b543a6079fe70ab2d33
Memi Beltrame <memi.beltrame@liip.ch>
2011-08-03 00:01:06 +0200

    add ticketting

--- apps/frontend/modules/tickets/templates/indexSuccess.php
+++ apps/frontend/modules/tickets/templates/indexSuccess.php
@@ -1,35 +1,7 @@
-<div class="row">
+<div class="row lastBeforeFooter">
     <div class="column grid_12">
         <h2>Tickets</h2>
-    </div>
-</div>
-<div class="row lastBeforeFooter">
-    <div class="column grid_4">
-        We don't have a set price for tickets yet, since we are still negotiating with sponsors<br><br>
-        <div class="pointout">AFFORDABLE TICKETS is one of our top priorities!</div>
-        <br><br>
-
-    </div>
-    <div class="column grid_1">
-        &nbsp;
-    </div>
-    <div class="column grid_4">
-        <div class="infobox">
-            <h3>Get Notified</h3>
-            Leave us your email-address and we will inform you once we're ready.<br>
-            <form action ="/tickets/rememberMe/" method="post"><br>
-                <label for="email">Email<span class="mandatory">★ mandatory</span></label>
-                <input type="email" class="maxWidth" name="email" required autofocus>
-                    <br><br>
-                <input class="button" type="submit" value="Subscribe">
-                <br><br><br>
-                <div class="decoDivider">or</div>
-                <a href="http://twitter.com/frontendconfch">Follow us on Twitter @frontendconfch</a>
-
-            </form>
-        </div>
-    </div>
-        <div class="column grid_3">
-        &nbsp;
+        We have 70 early bird tickets available so get them while they're hot!<br><br>
+        <div style="width:100%; text-align:left;" ><iframe  src="http://www.eventbrite.com/tickets-external?eid=1453745191&ref=etckt" frameborder="0" height="274" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com/r/etckt" >Online Ticketing</a><span style="color:#ddd;" > for </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com/event/1453745191?ref=etckt" >Frontend Conference Zurich</a><span style="color:#ddd;" > powered by </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com?ref=etckt" >Eventbrite</a></div></div>
     </div>
 </div>
