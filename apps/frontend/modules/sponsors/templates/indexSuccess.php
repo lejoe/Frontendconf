f0f564e095e8f683b97ae68e99ae03147909ca19
Memi Beltrame <memi.beltrame@liip.ch>
2011-08-05 11:34:03 +0200

    sponsors logos and page

--- apps/frontend/modules/sponsors/templates/indexSuccess.php
+++ apps/frontend/modules/sponsors/templates/indexSuccess.php
@@ -3,31 +3,109 @@
         <h2>Sponsors</h2>
     </div>
 </div>
-<div class="row lastBeforeFooter">
+<div class="row">
     <div class="column grid_8">
         <div class="lead">
-            Our goal is to initiate and establish the Frontend Conference Zurich  as a leading conference about state-of-the-art frontend and UX technologies in Europe and as the leading frontend/UX conference in Switzerland.<br><br>
+            The Frontend Conference Zurich was made possible thanks to the awesome contributions of the following companies:<br><br>
         </div>
      </div>
     <div class="column grid_4">
-        <div class="infobox">
-            <h3>Get all the Sponsor Info</h3>
-
-            All information on the benefits, sponsorship options and pricing.<br><br>
-
-            <input class="button" type="submit" value="Sponsors Info PDF">
-            <br><br>
-            <h4>Sponsoring contact</h4>
-            Markus Leutwyler<br>
-            markus < at > frontendconf.ch<br>
-            twitter.com/twtomcat<br>
-        </div>
+        &nbsp;
     </div>
 </div>
 <!-- Platinum -->
-<div class="row lastBeforeFooter">
+<div class="row">
     <div class="column grid_8">
-        <h3>Platinum</h3>
+        <h3>Platinum Sponsors</h3>
+    </div>
+    <div class="column grid_4">
+        &nbsp;
+    </div>
+</div>
+<div class="row sponsorRow">
+    <div class="column grid_3">
+        <img src="/images/sponsors/netcetera.gif" alt="Netcetera is one of our two Platinum Sponsors">
+    </div>
+    <div class="column grid_7">
+        Netcetera is one of the top IT service providers in Switzerland. Its proven expertise and extensive experience make it a trustworthy partner for Swiss and international organizations for major security and mission-critical IT projects.
+        <br><a href="http://netcetera.ch">Netcetera Website</a>
+    </div>
+    <div class="column grid_2">
+        &nbsp;
+    </div>
+</div>
+<div class="row ">
+    <div class="column grid_3">
+        <img src="/images/sponsors/unic.gif" alt="Unic is one of our two Platinum Sponsors">
+    </div>
+    <div class="column grid_7">
+        Unic is one of Switzerland's leading Internet Solutions Providers with offices in Amsterdam, Berne, Karlsruhe, Vienna and Zurich. Unic designs, develops and maintains E-Business and Enterprise Content Management solutions for a growing number of established enterprises.
+        <br><a href="http://netcetera.ch">Unic Website</a>
+    </div>
+    <div class="column grid_2">
+        &nbsp;
+    </div>
+</div>
+<div class="row speakerRow">
+    <div class="column grid_12 speakerRowBottom"> &nbsp;</div>
+</div>
+<!-- Gold -->
+<div class="row">
+    <div class="column grid_8">
+        <br><br>
+        <h3>Gold Sponsors</h3>
+    </div>
+    <div class="column grid_4">
+        &nbsp;
+    </div>
+</div>
+
+
+<div class="row sponsorRow">
+    <div class="column grid_3">
+        <img src="/images/sponsors/hostpoint.gif" alt="Hostpoint is one of our Gold Sponsors"><br><br>
+    </div>
+    <div class="column grid_7">
+        With 130,000 websites and 150,000 domain names, Hostpoint is Switzerland's leading web hosting provider. We offer low-priced web space and register our customer’s domain names worldwide. Our support staff is also there whenever and wherever support may be needed. Hostpoint helps you venture out into the Internet with ease and confidence – be it with our own Content Management System web-o-mat, TYPO3 or Joomla! And thanks to the many additional services and the modular design of our offerings, you can build and expand your website to your specific liking. Register your domain now and reserve your web hosting with Hostpoint.
+        <br><a href="http://liip.ch">Liip Website</a>
+    </div>
+    <div class="column grid_2">
+        &nbsp;
+    </div>
+</div>
+<div class="row sponsorRow">
+    <div class="column grid_3">
+        <img src="/images/sponsors/liip.gif" alt="Liip is one of our Gold Sponsors"><br><br>
+    </div>
+    <div class="column grid_7">
+        Liip is Switzerland’s leading developer of tailor-made, agile web applications. State-of-the-art technologies, tried and tested industry standards and open-source software are the foundations on which our services are built. Developing new concepts and breaking new ground are all in a day’s work for us. We have built an organisational culture that is socially and environmentally sustainable.
+        <br><a href="http://liip.ch">Liip Website</a>
+    </div>
+    <div class="column grid_2">
+        &nbsp;
+    </div>
+</div>
+<div class="row">
+    <div class="column grid_3">
+        <img src="/images/sponsors/nine.gif" alt="Nine is one of our Gold Sponsors"><br><br>
+    </div>
+    <div class="column grid_7">
+        Nine Internet Solutions AG is an Internet provider that is specialised in tailor-made hosting and server solutions based in Zurich, Switzerland.
+        <br><a href="http://liip.ch">Liip Website</a>
+    </div>
+    <div class="column grid_2">
+        &nbsp;
+    </div>
+</div>
+<div class="row speakerRow">
+    <div class="column grid_12 speakerRowBottom"> &nbsp; </div>
+</div>
+
+<!-- Silver -->
+<div class="row">
+    <div class="column grid_8">
+        <br><br>
+        <h3>Silver Sponsors</h3>
     </div>
     <div class="column grid_4">
         &nbsp;
@@ -35,11 +113,11 @@
 </div>
 <div class="row lastBeforeFooter">
     <div class="column grid_3">
-        <img src="/images/logo_acme.png">
+        <img src="/images/sponsors/unic.gif" alt="Unic is one of our two Platinum Sponsors"><br><br>
     </div>
     <div class="column grid_7">
-        <h3>Acme</h3>
-        Lorem Ipsum
+        Unic is one of Switzerland's leading Internet Solutions Providers with offices in Amsterdam, Berne, Karlsruhe, Vienna and Zurich. Unic designs, develops and maintains E-Business and Enterprise Content Management solutions for a growing number of established enterprises.
+        <br><a href="http://netcetera.ch">Unic Website</a>
     </div>
     <div class="column grid_2">
         &nbsp;
