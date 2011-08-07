912dfb335890680be98d3002fcd8e018d7858a8d
Memi Beltrame <memi.beltrame@liip.ch>
2011-08-07 11:06:15 +0200

    move proposal form

--- apps/frontend/modules/propose/templates/indexSuccess.php
+++ apps/frontend/modules/propose/templates/indexSuccess.php
@@ -4,71 +4,8 @@
     </div>
 </div>
 <div class="row lastBeforeFooter">
-    <div class="column grid_6">
-        <form action="/propose/add/"  method="post">
-        <fieldset>
-            <legend>Tell us about Your Talk</legend>
-            <p class="fsContent">
-
-                <label for="title">Talk title <span class="mandatory">★ mandatory</span></label>
-                <input type="text" name="title" class="maxWidth" autofocus required><br>
-                <label for="abstract">Abstract <span class="mandatory">★ mandatory</span></label>
-                <textarea name="abstract" class="maxWidth" rows="15" required></textarea>
-            </p>
-        </fieldset>
-
-        <fieldset>
-            <legend>Tell us about you</legend>
-            <p class="fsContent">
-                <label for="name">Name <span class="mandatory">★ mandatory</span></label>
-                <input type="text" name="name" class="maxWidth" required><br>
-                <label for="name">Email <span class="mandatory">★ mandatory</span></label>
-                <input type="email" name="email" class="maxWidth" required><br>
-                <label for="bio">A short bio <span class="mandatory">★ mandatory</span></label>
-                <textarea name="bio" class="maxWidth" rows="5" required></textarea>
-                <label for="profession">Profession</label>
-                <input type="text" name="profession" class="maxWidth"><br>
-                <label for="name">Company</label>
-                <input type="text" name="company" class="maxWidth"><br>
-                <label for="name">Twitter-Name</label>
-                <input type="text" name="twitter" class="maxWidth"><br>
-                <label for="name">Your Website</label>
-                <input type="url" value="http://"name="website" class="maxWidth"><br>
-            </p>
-        </fieldset>
-
-        <fieldset>
-            <legend>Organizational Stuff</legend>
-            <p class="fsContent">
-                <label for="datePreferenceForTalk">Do you have a preference for the day of your talk?</label>
-                <input type="radio" name="datePreferenceForTalk" value="None"> None<br>
-                <input type="radio" name="datePreferenceForTalk" value="Friday"> Friday<br>
-                <input type="radio" name="datePreferenceForTalk" value="Saturday"> Saturday <br><br>
-                <label for="costsCovered">Does your company pay for your expenses?</label>
-                <input type="radio" name="costsCovered" value="Yes"> Yes<br>
-                <input type="radio" name="costsCovered" value="Some"> Some<br>
-                <input type="radio" name="costsCovered" value="No"> No<br>
-            </p>
-        </fieldset>
-        <br>
-        <input class="button" type="submit" value="Submit Proposal">
-        </form>
+    <div class="column grid_12">
+        Proposals are now closed - <a href="/tickets">Join us as an attendee!</a>
     </div>
-    <div class="column grid_1">
-        &nbsp;
-    </div>
-    <div class="column grid_5">
-        <h3>Deadlines:</h3>
-        <strong class="bold">7th of August, midnight:</strong> End of the call for talks<br>
-        <strong class="bold">Until 13th of August:</strong> We will contact everyone that has submitted a talk proposal
-        <br><br>
-        <h3>Speaker Info</h3>
-        If you plan on speaking at the Conference you might want to know the following:<br>
-        <ul  class="point">
-            <li>We will cover your traveling costs and offer you accommodation.</li>
-            <li>We can't pay you a salary or fee for giving the talk or preparing it</li>
-            <li>We're in it for the love of it, and don't make any money.</li>
-            <li>If your company can support you by covering some of your expenses we are very thankful - we can use every dime. In return this entitles your company to a "Contributor Sponsorship"</li>
-        </ul>
-    </div>
+
 </div>
