7e9a2ea9f91a3eb86bccafdee33ff55ba4c17f28
Memi Beltrame <memi.beltrame@liip.ch>
2011-08-07 11:06:29 +0200

    add sponsor us button

--- apps/frontend/modules/sponsors/templates/indexSuccess.php
+++ apps/frontend/modules/sponsors/templates/indexSuccess.php
@@ -4,12 +4,15 @@
     </div>
 </div>
 <div class="row">
-    <div class="column grid_8">
+    <div class="column grid_6">
         <div class="lead">
             The Frontend Conference Zurich was made possible thanks to the awesome contributions of the following companies:<br><br>
         </div>
      </div>
     <div class="column grid_4">
+        <a class="button" href="/sponsoring">Sponsor Us!</a>
+    </div>
+    <div class="column grid_2">
         &nbsp;
     </div>
 </div>
