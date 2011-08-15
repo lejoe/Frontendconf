04f4bc3eaf2ae48bbfa6b22aa6b8d4fcc09b48b7
Memi Beltrame <memi.beltrame@liip.ch>
2011-08-15 05:27:02 +0200

    info page

---
+++ apps/frontend/modules/info/templates/indexSuccess.php
@@ -0,0 +1,29 @@
+<div class="row">
+    <div class="column grid_12">
+        <h2>Visitor Info</h2>
+    </div>
+</div>
+
+<div class="row">
+    <div class="column grid_6">
+        <h3>Getting to the Conference</h3>
+        The Address of the Conference Venue is:<br><br>
+        University of Zurich<br>
+        Department of Informatics <br>
+        Binzmühlestrasse 14<br>
+        CH-8050 Zurich<br>
+<br>
+        <h4>From the Airport</h4>
+        <span class="lineheader10"><b>Tram Line 10 to Zürich Leutschenbach</b></span><br>
+                Direct tram connection from the airport to the conference:<br>
+                Take Tram Nr 10 (the pink line) from the Airport in direction Bahnhofplatz/HB and get out at the stop called "Zürich Leutschenbach".<br>
+                This is a 12 minute ride.<br><br>
+                From the tram stop it is a short walk across the street.<br><br>
+
+                <iframe class="mapStyles" width="300" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.ch/maps?f=d&amp;source=s_d&amp;saddr=z%C3%BCrich+leutschenbach+haltestelle&amp;daddr=47.414319,8.549686&amp;hl=de&amp;geocode=FZB80wIdlHuCACnzHzQkgwqQRzHAq83kzw3EQg%3BFS980wIdNnWCAA&amp;mra=ltm&amp;dirflg=w&amp;sll=47.41726,8.553115&amp;sspn=0.008465,0.02178&amp;ie=UTF8&amp;ll=47.414375,8.550925&amp;spn=0.001815,0.003208&amp;z=17&amp;output=embed"></iframe><br /><small><a href="http://maps.google.ch/maps?f=d&amp;source=embed&amp;saddr=z%C3%BCrich+leutschenbach+haltestelle&amp;daddr=47.414319,8.549686&amp;hl=de&amp;geocode=FZB80wIdlHuCACnzHzQkgwqQRzHAq83kzw3EQg%3BFS980wIdNnWCAA&amp;mra=ltm&amp;dirflg=w&amp;sll=47.41726,8.553115&amp;sspn=0.008465,0.02178&amp;ie=UTF8&amp;ll=47.414375,8.550925&amp;spn=0.001815,0.003208&amp;z=17" style="color:#0000FF;text-align:left">Open in Google Maps</a></small>
+
+    </div>
+    <div class="column grid_6">
+        <h3>Accomodation</h3>
+    </div>
+</div>E80089
