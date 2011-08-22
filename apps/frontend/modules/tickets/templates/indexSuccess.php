3ee6c39526efc337928feb310cb1ad96ab08461e
lejoe <copernic67@lejoe.com>
2011-08-22 13:19:57 +0200

    Removed the "no more early brids tickets" disclaimer

--- apps/frontend/modules/tickets/templates/indexSuccess.php
+++ apps/frontend/modules/tickets/templates/indexSuccess.php
@@ -1,7 +1,6 @@
 <div class="row lastBeforeFooter">
     <div class="column grid_12">
         <h2>Tickets</h2>
-        There are no more early bird tickets. Regular Tickets will be on sale until September 4th at 12PM <br><br>
         <div style="width:100%; text-align:left;" ><iframe  src="http://www.eventbrite.com/tickets-external?eid=1453745191&ref=etckt" frameborder="0" height="274" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com/r/etckt" >Online Ticketing</a><span style="color:#ddd;" > for </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com/event/1453745191?ref=etckt" >Frontend Conference Zurich</a><span style="color:#ddd;" > powered by </span><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com?ref=etckt" >Eventbrite</a></div></div>
     </div>
 </div>
