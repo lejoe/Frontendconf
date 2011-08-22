e90ad0465ab60a6d6d931b900568f40689b9b348
lejoe <copernic67@lejoe.com>
2011-08-22 06:43:05 +0200

    Styled the programme for the mobile. Started to style it for broadscreen

--- apps/frontend/modules/programme/templates/_slot.php
+++ apps/frontend/modules/programme/templates/_slot.php
@@ -1,13 +1,9 @@
-<a href="/speakers#id<?php echo $speaker['id'];?>"><?php echo $speaker['Talks'][0]['title'];?></a>
+
 
                 <div class="row speakerEntry">
+                    <a href="/speakers#id<?php echo $speaker['id'];?>"><?php echo $speaker['Talks'][0]['title'];?></a>
                     <div class="column innergrid_1">
                         <img width="52" height="52" class="person" alt="<?php echo $speaker['name'];?>" src="/images/speakers/thumb_<?php echo $speaker['img'];?>.jpg">
                     </div>
-                    <div class="column grid_2">
-                        <h4><?php echo $speaker['name'];?></h4>
-                        <?php if (!empty($speaker['profession'])) {
-                            //echo " " . $speaker['profession'];?>
-                        <?php } ?>
-                    </div>
+                    <h4><?php echo $speaker['name'];?></h4>
                 </div>
