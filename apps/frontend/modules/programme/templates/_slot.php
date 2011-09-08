fcc827605e1144effa278464573d34d8e2b179b1
lejoe <copernic67@lejoe.com>
2011-09-08 15:32:30 +0200

    Added the track name to the talks

--- apps/frontend/modules/programme/templates/_slot.php
+++ apps/frontend/modules/programme/templates/_slot.php
@@ -1,11 +1,14 @@
 
 
                 <div class="row speakerEntry">
+                    <div class="topic"><?php echo $speaker['Talks'][0]['topic'];?>: </div>
                     <a href="/speakers#id<?php echo $speaker['id'];?>"><?php echo $speaker['Talks'][0]['title'];?></a>
+                    
                     <?php if (!empty($speaker['img'])) {?>
                       <div class="column innergrid_1">
                         <img width="52" height="52" class="person" alt="<?php echo $speaker['name'];?>" src="/images/speakers/thumb_<?php echo $speaker['img'];?>.jpg">
+                        <h4 class="name"><?php echo $speaker['name'];?></h4>
                       </div>
                     <?php } ?>
-                    <h4><?php echo $speaker['name'];?></h4>
+                    
                 </div>
